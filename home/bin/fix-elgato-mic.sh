48fcf4c8e88e85412bd508d8ce45c142bd15ba33
Manuel Viera <manuel.viera@crononauta.com>
2022-11-05 23:44:57 +0100

    Fixes Elgato Wave:3 mic

---
+++ home/bin/fix-elgato-mic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This will set the Elgato Wave:3 profile to 'Mono' and then back to 'Digital
+# Stereo (IEC958) Output + Mono Input' to get it working again.
+
+CARD_INDEX=$(pacmd list-cards |grep -iE "name:.*elgato.*" -B1 | grep index |awk -F":" '{ print $2}' |sed 's/ //g')
+pacmd set-card-profile $CARD_INDEX input:mono-fallback
+pacmd set-card-profile $CARD_INDEX output:iec958-stereo+input:mono-fallback
